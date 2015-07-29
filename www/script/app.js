3f005bcddcf1ae931c1022897221e15a267a67d2
adtiv <datocco13@gmail.com>
2015-07-29 19:25:05 -0400

    added fastclick library to increase app speed
    still need to apply to the rest of the pages

--- www/script/app.js
+++ www/script/app.js
@@ -61,6 +61,7 @@ function storeLocalContent(){
 
 }
 	document.addEventListener('deviceready', function () {
+		FastClick.attach(document.body);
 		console.log("deviceReady");	
 	    // cordova.plugins.email is now available
 	}, false);
