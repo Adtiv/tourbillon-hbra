49032eb743360869a6d95cac4c1c1ce54031d051
adtiv <datocco13@gmail.com>
2015-08-13 18:59:52 -0400

    redid change

--- platforms/ios/www/script/jsPDF-master/plugins/from_html.js
+++ platforms/ios/www/script/jsPDF-master/plugins/from_html.js
@@ -793,14 +793,14 @@
 		var test2=this.y + this.pdf.internal.getFontSize();
 		this.pdf.internal.write("pageSize: "+test1+" textheight: "+ test2);
 		if (this.pdf.internal.pageSize.height - this.pdf.margins_doc.bottom < this.y + this.pdf.internal.getFontSize()) {
-			//text = "                    "+text; //"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t "
+			text = "                    "+text; //"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t "
 			this.pdf.internal.write("ET", "Q");
 			
 			this.pdf.addPage();
 			this.y = this.pdf.margins_doc.top;
 			this.pdf.internal.write("q", "BT 0 g", this.pdf.internal.getCoordinateString(this.x), this.pdf.internal.getVerticalCoordinateString(this.y), style.color, "Td");
 			//move cursor by one line on new page
-			maxLineHeight = Math.max(maxLineHeight, style["line-height"], style["font-size"]);//-67.8;
+			maxLineHeight = Math.max(maxLineHeight, style["line-height"], style["font-size"])-67.8;
 			this.pdf.internal.write(0, (-1 * defaultFontSize * maxLineHeight).toFixed(2), "Td");
 			//this.pdf.pdfEscape(text);
 
