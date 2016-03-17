248d3deeae63a722f0580bba355233d059212ab3
Adtiv <datocco13@gmail.com>
2016-03-17 14:41:42 -0400

    Phonegap build update test

--- www/script/app.js
+++ www/script/app.js
@@ -22,7 +22,7 @@ function init(){
 		document.getElementById('weatherCom').value = window.localStorage.getItem('weatherCom');
 
 	}
-	
+	alert('DID IT UPDATE');
 }
 
 function storeLocalContent(next){
