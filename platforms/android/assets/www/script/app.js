2b6d79f6686934ac84facd2573b9d1a204bd9451
bjlavigne <brian.lavigne@att.net>
2017-05-13 06:58:05 -0400

    Sprint 31 - Language popup and Datetimepicker Chgs
    1) "Coming Soon" popup when unsupported languages are selected, and
    2) AM/PM display improvements for the Datetimepicker - using local one

--- platforms/android/assets/www/script/app.js
+++ platforms/android/assets/www/script/app.js
@@ -7,18 +7,3 @@
 		//window.alert("app.js  - inpectNumber does NOT exist: " + elementExists);
 	}
 	};
-
-	function AppIntialize() {
-	try {
-	document.addEventListener('deviceready', function () {
-		FastClick.attach(document.body);
-		//window.alert("App.js - Listener Added Now");
-	    // cordova.plugins.email is now available
-	}, false);
-	} catch (e) {
-		window.alert("App.js AddEvent Error: " + e.message);
-	}
-	//window.alert("App.js - After Loaded");
-	}
-
-	AppIntialize();
